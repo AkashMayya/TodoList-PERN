fe08666371c3c1f8093a53ad1bd93492e26e4631
Akash Mayya <mayyaakash19@gmail.com>
2023-11-25 04:56:19 +0530

    standard prototype

--- server/db.js
+++ server/db.js
@@ -1,10 +1,11 @@
-const Pool =require("pg").Pool;
-const pool=new Pool({
-    user: "postgres",
-    password:"postgres",
-    host:"Localhost",
-    port:5432,
-    database:"perntodo"
+const Pool = require("pg").Pool;
+
+const pool = new Pool({
+  user: "postgres",
+  password: "postgres",
+  host: "localhost",
+  port: 5432,
+  database: "perntodo"
 });
 
-module.exports=pool;
+module.exports = pool;
